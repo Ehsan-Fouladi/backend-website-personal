ef209a7ccb3f7bc90014edf462d5bda4541d4780
Ehsan-Fouladi <fooehan3@gmail.com>
2025-02-11 04:00:24 +0330

    added new route tag

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+require("dotenv").config();
+require("./src/server");
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT ?? 4000;
+const fs = require("fs");
+const path = require("path");
+const uploadDir = path.join(__dirname, "uploads");
 const articleRouter = require("./src/routers/articleRouter");
-require("./src/server");
-require("dotenv").config();
-const swaggerDocs = require("./src/swagger");
+const tagRouter = require("./src/routers/tagRouter");
 
-swaggerDocs(app);
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+  console.log("📂 Upload File Created");
+}
 
 app.use(express.json());
-app.use("/", articleRouter); 
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", articleRouter);
+app.use("/api", tagRouter);
+
+app.use("/uploads", express.static("uploads"));
 
 app.listen(port, () => {
   console.log(`🚀 Server running on http://localhost:${port}`);
